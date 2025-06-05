0cd93e0e93d40ef81c78786ff69c1991518fa628
DmitryTL <Dmitry.plus1@yandex.ru>
2025-06-05 17:22:39 +0600

    Created new ENUM

---
+++ src/main/java/org/LukDT/enums/StudentComparatorOptions.java
@@ -0,0 +1,5 @@
+package org.LukDT.enums;
+
+public enum StudentComparatorOptions {
+    FULL_NAME, UNIVERSITY_ID, CURRENT_COURSE_NUMBER, AVG_EXAM_SCOPE;
+}
---
+++ src/main/java/org/LukDT/enums/UniversityComparatorOptions.java
@@ -0,0 +1,5 @@
+package org.LukDT.enums;
+
+public enum UniversityComparatorOptions {
+    ID, FULL_NAME, SHORT_NAME, YEAR_OF_FOUNDATION, MAIN_PROFILE;
+}
