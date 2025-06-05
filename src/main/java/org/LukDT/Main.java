7d987c822e145484b3e6f79ec0d40999ccc6be3d
DmitryTL <Dmitry.plus1@yandex.ru>
2025-06-05 18:11:13 +0600

    Last commit

--- src/main/java/org/LukDT/Main.java
+++ src/main/java/org/LukDT/Main.java
@@ -1,8 +1,14 @@
 package org.LukDT;
 
 import org.LukDT.File.ReadXlsx;
+import org.LukDT.comparatorModel.student.StudentComparator;
+import org.LukDT.comparatorModel.university.UniversityComparator;
+
+import org.LukDT.enums.StudentComparatorOptions;
+import org.LukDT.enums.UniversityComparatorOptions;
 import org.LukDT.model.Student;
 import org.LukDT.model.University;
+import org.LukDT.privateClass.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +16,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>(ReadXlsx.readStudent());
+        List<University> universities = new ArrayList<>(ReadXlsx.readUniversity());
 
-       List<University> universities = new ArrayList<>(ReadXlsx.readUniversity());
+        StudentComparator studentComparator = Utils.getStudentComparator(StudentComparatorOptions.FULL_NAME);
+        UniversityComparator universityComparator = Utils.getUniversityComparator(UniversityComparatorOptions.SHORT_NAME);
 
-        for(Student s : students) {
-            System.out.println(s);
-        }
-
+        students.stream().sorted(studentComparator).forEach(System.out::println);
+        universities.stream().sorted(universityComparator).forEach(System.out::println);
     }
 }
