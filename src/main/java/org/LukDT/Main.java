828e6f1720a79a54fba9858a5efb14e6365bbe26
DmitryTL <Dmitry.plus1@yandex.ru>
2025-06-10 20:14:29 +0600

    Update Class JsonUtil

--- src/main/java/org/LukDT/Main.java
+++ src/main/java/org/LukDT/Main.java
@@ -16,20 +16,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Создаем коллекции полученных данный из xlsx
         List<Student> students = new ArrayList<>(ReadXlsx.readStudent());
         List<University> universities = new ArrayList<>(ReadXlsx.readUniversity());
 
+        //Сортируем объекты при помощи компаратора
         StudentComparator studentComparator = Utils.getStudentComparator(StudentComparatorOptions.FULL_NAME);
         UniversityComparator universityComparator = Utils.getUniversityComparator(UniversityComparatorOptions.SHORT_NAME);
 
+        //Выводем отсортированые объекты
         students.stream().sorted(studentComparator).forEach(System.out::println);
+        System.out.println("---------------------");
         universities.stream().sorted(universityComparator).forEach(System.out::println);
 
-
+        //Выполняем сериализацию и десериализацию отдельных объектов и коллекций
         JsonUtil.StudentSerialization(students.get(1));
         System.out.println(JsonUtil.StudentDeserialization());
 
         JsonUtil.UniversitySerialization(universities.get(0));
         System.out.println(JsonUtil.UniversityDeserialization());
+
+        JsonUtil.StudentCollectionSerialization(students);
+        System.out.println(JsonUtil.StudentCollectionDeserialization());
+
+        System.out.println("---------------------");
+
+        JsonUtil.UniversityCollectionSerialization(universities);
+        System.out.println(JsonUtil.UniversityCollectionDeserialization());
     }
 }
--- src/main/java/org/LukDT/json/JsonUtil.java
+++ src/main/java/org/LukDT/json/JsonUtil.java
@@ -2,17 +2,43 @@ package org.LukDT.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import org.LukDT.model.Student;
 import org.LukDT.model.University;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
-public final class JsonUtil {
-    private JsonUtil() {
 
+public final class JsonUtil {
+    private JsonUtil() {}
+
+    public static void StudentCollectionSerialization(List<Student> list) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try(FileWriter writer = new FileWriter("studentCollection.json")) {
+            gson.toJson(list, writer);
+            System.out.println("Коллекция сериализованна");
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Student> StudentCollectionDeserialization() {
+        Gson gson = new Gson();
+
+        try(FileReader reader = new FileReader("studentCollection.json")) {
+            Type type = new TypeToken<List<Student>>(){}.getType();
+            List<Student> list = gson.fromJson(reader, type);
+
+            return list;
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static Student StudentDeserialization() {
@@ -39,6 +65,32 @@ public final class JsonUtil {
 
         System.out.println("Сериализованный JSON:\n" + gson.toJson(s));
     }
+
+    public static void UniversityCollectionSerialization(List<University> universities) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try(FileWriter writer = new FileWriter("universityCollection.json")) {
+            gson.toJson(universities, writer);
+            System.out.println("Коллекция сериализованна");
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<University> UniversityCollectionDeserialization() {
+        Gson gson = new Gson();
+
+        try(FileReader reader = new FileReader("universityCollection.json")) {
+            Type type = new TypeToken<List<University>>(){}.getType();
+            List<University> list = gson.fromJson(reader, type);
+
+            return list;
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     public static University UniversityDeserialization() {
         Gson gson = new Gson();
