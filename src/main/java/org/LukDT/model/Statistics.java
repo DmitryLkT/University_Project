6425d87d59abb8d19ea6c2dc9f85bccbfaaa6e80
DmitryTL <Dmitry.plus1@yandex.ru>
2025-06-17 16:15:18 +0600

    Create class Statistics

---
+++ src/main/java/org/LukDT/model/Statistics.java
@@ -0,0 +1,60 @@
+package org.LukDT.model;
+
+import org.LukDT.enums.StudyProfile;
+
+public class Statistics {
+    private StudyProfile statisticsProfile;
+    private float statisticsAvg;
+    private int statisticsCountStudents;
+    private int statisticsCountUniversity;
+    private String statisticsNameUniversity;
+
+    public Statistics(StudyProfile statisticsProfile, float statisticsAvg, int statisticsCountStudents,
+                      int statisticsCountUniversity, String statisticsNameUniversity) {
+        this.statisticsProfile = statisticsProfile;
+        this.statisticsAvg = statisticsAvg;
+        this.statisticsCountStudents = statisticsCountStudents;
+        this.statisticsCountUniversity = statisticsCountUniversity;
+        this.statisticsNameUniversity = statisticsNameUniversity;
+    }
+
+    public StudyProfile getStatisticsProfile() {
+        return statisticsProfile;
+    }
+
+    public void setStatisticsProfile(StudyProfile statisticsProfile) {
+        this.statisticsProfile = statisticsProfile;
+    }
+
+    public float getStatisticsAvg() {
+        return statisticsAvg;
+    }
+
+    public void setStatisticsAvg(float statisticsAvg) {
+        this.statisticsAvg = statisticsAvg;
+    }
+
+    public int getStatisticsCountStudents() {
+        return statisticsCountStudents;
+    }
+
+    public void setStatisticsCountStudents(int statisticsCountStudents) {
+        this.statisticsCountStudents = statisticsCountStudents;
+    }
+
+    public int getStatisticsCountUniversity() {
+        return statisticsCountUniversity;
+    }
+
+    public void setStatisticsCountUniversity(int statisticsCountUniversity) {
+        this.statisticsCountUniversity = statisticsCountUniversity;
+    }
+
+    public String getStatisticsNameUniversity() {
+        return statisticsNameUniversity;
+    }
+
+    public void setStatisticsNameUniversity(String statisticsNameUniversity) {
+        this.statisticsNameUniversity = statisticsNameUniversity;
+    }
+}
