b652b5e68bfdcf4c690f914534e4e32a596eb6d9
DmitryTL <Dmitry.plus1@yandex.ru>
2025-06-26 15:44:25 +0600

    Create logging.properties

--- src/main/java/org/LukDT/Main.java
+++ src/main/java/org/LukDT/Main.java
@@ -27,49 +27,20 @@ public class Main {
         StudentComparator studentComparator = SortedUtils.getStudentComparator(StudentComparatorOptions.FULL_NAME);
         UniversityComparator universityComparator = SortedUtils.getUniversityComparator(UniversityComparatorOptions.SHORT_NAME);
 
-        //Выводем отсортированые объекты
-        students.stream().sorted(studentComparator).forEach(System.out::println);
-        System.out.println("---------------------");
-        universities.stream().sorted(universityComparator).forEach(System.out::println);
-
         //Выполняем сериализацию и десериализацию отдельных объектов и коллекций
-        JsonUtils.StudentSerialization(students.get(1));
-        System.out.println(JsonUtils.StudentDeserialization());
-
-        JsonUtils.UniversitySerialization(universities.get(0));
-        System.out.println(JsonUtils.UniversityDeserialization());
-
-        System.out.println(JsonUtils.StudentCollectionSerialization(students));
-        System.out.println(JsonUtils.StudentCollectionDeserialization());
-
-        System.out.println("---------------------");
-
-        System.out.println(JsonUtils.UniversityCollectionSerialization(universities));
-        System.out.println(JsonUtils.UniversityCollectionDeserialization());
-
-
+        JsonUtils.StudentSerialization(students.get(1));;
 
         universities.stream()
                 //Выбираем отдельные объекты, которые основали полсе 1980 года
                 .filter(u -> u.getYearOfFoundation() > 1980).
                 forEach(u -> {
-                    //Выводим получившиеся JSON-строки
-                    System.out.println(JsonUtils.UniversitySerialization(u));
-                    //Выводим десериализованный объект
-                    System.out.println(JsonUtils.UniversityDeserialization());
         });
 
         students.stream()
                 //Выбираем студентов 1-го курса
                 .filter(s -> s.getCurrentCourseNumber() == 1)
                 .forEach(s -> {
-                    //Выводим получившиеся JSON-строки
-                    System.out.println(JsonUtils.StudentSerialization(s));
-                    //Выводим десериализованный объект
-                    System.out.println(JsonUtils.StudentDeserialization());
                 });
-
-        System.out.println("---------------------");
 
         //Вызываем обработку статистики
         List<Statistics> statistics = CollectionProcessingUtils.createStatistics(students, universities);
